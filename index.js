12848f138d9f90e13f234daa8592267619f29efb
Anders Christmann <Anders@christmann.dk>
2020-06-15 00:31:22 +0200

    Added a signup and login functionality. Login session is yet to be added. Passwords are encrypted with bcrypt and stored on the database.

--- index.js
+++ index.js
@@ -8,6 +8,11 @@ const storePostController = require('./controllers/storePost')
 const homeController = require('./controllers/home')
 const getPostController = require('./controllers/getPost')
 const validateMiddleWare = require('./middleware/validationMiddleware')
+const newUserController = require('./controllers/newUser')
+const storeUserController = require('./controllers/storeUser')
+const loginController = require('./controllers/login')
+const loginUserController = require('./controllers/loginUser')
+
 
 
 mongoose.connect('mongodb://localhost/my_database', {useNewUrlParser: true})
@@ -34,6 +39,13 @@ app.get('/post/:id', getPostController)
 
 app.get('/posts/new', newPostController)
 
+app.get('/auth/register', newUserController)
+
+app.post('/users/register', storeUserController)
+
+app.get('/auth/login', loginController)
+
+app.post('/users/login', loginUserController)
 
 app.listen(4000, ()=>{
     console.log('App listening on port 4000')
