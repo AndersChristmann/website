9e53f26c4e1825ab4ea5e083af9a62512471549e
Anders Christmann <Anders@christmann.dk>
2020-06-15 01:32:54 +0200

    Added session to logins, added logout functionality, added 404 page not found

--- index.js
+++ index.js
@@ -1,19 +1,28 @@
+//Modules
 const express = require('express')
 const mongoose = require('mongoose')
 const bodyParser = require('body-parser')
 const ejs = require('ejs')
 const fileUpload = require('express-fileupload')
+
+//Controllers
 const newPostController = require('./controllers/newPost')
 const storePostController = require('./controllers/storePost')
 const homeController = require('./controllers/home')
 const getPostController = require('./controllers/getPost')
-const validateMiddleWare = require('./middleware/validationMiddleware')
 const newUserController = require('./controllers/newUser')
 const storeUserController = require('./controllers/storeUser')
 const loginController = require('./controllers/login')
 const loginUserController = require('./controllers/loginUser')
+const logoutController = require('./controllers/logout')
 
+//Middleware
+const validateMiddleWare = require('./middleware/validationMiddleware')
+const expressSession = require('express-session')
+const authMiddleWare = require('./middleware/authMiddleware')
+const redirectIfAuthenticatedMiddlewWare = require('./middleware/redirectIfAuthenticatedMiddleware')
 
+global.loggedIn = null;
 
 mongoose.connect('mongodb://localhost/my_database', {useNewUrlParser: true})
 
@@ -31,21 +40,34 @@ app.use(fileUpload())
 
 app.use('/posts/store', validateMiddleWare)
 
-app.post('/posts/store', storePostController)
+app.use(expressSession({
+    secret: 'important secret'
+}))
+
+app.use("*", (req, res, next) => {
+    loggedIn = req.session.userId;
+    next()
+});
+
+app.post('/posts/store', authMiddleWare, storePostController)
 
 app.get('/', homeController)
 
 app.get('/post/:id', getPostController)
 
-app.get('/posts/new', newPostController)
+app.get('/posts/new', authMiddleWare, newPostController)
 
-app.get('/auth/register', newUserController)
+app.get('/auth/register', redirectIfAuthenticatedMiddlewWare, newUserController)
 
-app.post('/users/register', storeUserController)
+app.post('/users/register', redirectIfAuthenticatedMiddlewWare, storeUserController)
 
-app.get('/auth/login', loginController)
+app.get('/auth/login', redirectIfAuthenticatedMiddlewWare, loginController)
 
-app.post('/users/login', loginUserController)
+app.post('/users/login', redirectIfAuthenticatedMiddlewWare, loginUserController)
+
+app.get('/auth/logout', logoutController)
+
+app.use((req, res) => res.render('notfound'));
 
 app.listen(4000, ()=>{
     console.log('App listening on port 4000')
