517fd301b7d7d26270b39d526cb639aa9b2d34f7
Anders Christmann <Anders@christmann.dk>
2020-06-15 15:55:07 +0200

    Linked the user and blogpost collections so that I can display what user created which post.

--- models/BlogPost.js
+++ models/BlogPost.js
@@ -4,7 +4,11 @@ const Schema = mongoose.Schema;
 const BlogPostSchema = new Schema({
     title: String,
     body: String,
-    username: String,
+    userid: {
+        type: mongoose.Schema.Types.ObjectID,
+        ref: 'User',
+        required: true
+    },
     datePosted:{
         type: Date,
         default: new Date()
